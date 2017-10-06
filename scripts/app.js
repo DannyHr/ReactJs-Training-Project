a20045d7c68ceac8933dea13deea94c6e8cfa8ff
DannyHr <dani.hr11@gmail.com>
2017-10-06 16:01:32 +0300

    Showing item preview on click

--- scripts/app.js
+++ scripts/app.js
@@ -5,21 +5,27 @@ var STRINGS = {
     'currency': 'BGN'
 };
 
-var ItemEntity = createReactClass({
-    displayName: 'ItemEntity',
+var ItemListEntity = createReactClass({
+    displayName: 'ItemListEntity',
 
     propTypes: {
         id: PropTypes.number.isRequired,
         name: PropTypes.string.isRequired,
         description: PropTypes.string,
-        price: PropTypes.number.isRequired
+        price: PropTypes.number.isRequired,
+        changeCurrentItem: PropTypes.func.isRequired,
+        togglePreviewVisibility: PropTypes.func.isRequired
+    },
+    changeItem: function () {
+        this.props.changeCurrentItem(this.props);
+        this.props.togglePreviewVisibility(true);
     },
     render: function () {
         return React.createElement(
             'li',
-            { className: 'item' },
+            { className: 'item', onClick: this.changeItem },
             React.createElement(
-                'h2',
+                'h3',
                 { className: 'item-name' },
                 this.props.name
             ),
@@ -46,16 +52,24 @@ var ItemsList = createReactClass({
     displayName: 'ItemsList',
 
     propTypes: {
-        items: PropTypes.array.isRequired
+        items: PropTypes.array.isRequired,
+        changeCurrentItem: PropTypes.func.isRequired,
+        togglePreviewVisibility: PropTypes.func.isRequired
     },
     render: function () {
-        var elementsToRender = this.props.items.map(function (item) {
-            return React.createElement(ItemEntity, _extends({}, item, { key: item.id }));
+        var self = this;
+        var elementsToRender = self.props.items.map(function (item) {
+            return React.createElement(ItemListEntity, _extends({}, item, { changeCurrentItem: self.props.changeCurrentItem, togglePreviewVisibility: self.props.togglePreviewVisibility, key: item.id }));
         });
 
         return React.createElement(
             'div',
             { className: 'items-list-container' },
+            React.createElement(
+                'h2',
+                null,
+                'Items List'
+            ),
             React.createElement(
                 'ul',
                 { className: 'items-list' },
@@ -65,17 +79,98 @@ var ItemsList = createReactClass({
     }
 });
 
-var ItemPreviewScreen = createReactClass({
-    displayName: 'ItemPreviewScreen',
+var ItemPreviewContainer = createReactClass({
+    displayName: 'ItemPreviewContainer',
 
     propTypes: {
-        item: PropTypes.object.isRequired
+        item: PropTypes.object.isRequired,
+        isShown: PropTypes.bool.isRequired
+    },
+    getDefaultProps: function () {
+        return {
+            item: {},
+            isShown: false
+        };
+    },
+    render: function () {
+        if (this.props.isShown) {
+            return React.createElement(
+                'div',
+                { className: 'item-preview-screen fullscreen-popup' },
+                React.createElement(
+                    'h2',
+                    { className: 'item-name' },
+                    this.props.item.name
+                ),
+                React.createElement(
+                    'div',
+                    { className: 'item-description' },
+                    this.props.item.description
+                ),
+                React.createElement(
+                    'div',
+                    { className: 'item-price' },
+                    this.props.item.price + ' ' + STRINGS['currency']
+                ),
+                React.createElement(
+                    'a',
+                    { href: 'products/' + this.props.item.id, className: 'item-link' },
+                    STRINGS['view-more']
+                )
+            );
+        } else {
+            return React.createElement('div', { className: 'item-preview-screen fullscreen-popup' });
+        }
+    }
+});
+
+var HomePageView = createReactClass({
+    displayName: 'HomePageView',
+
+    getInitialState: function () {
+        return {
+            currentItem: {},
+            allItems: items,
+            isPreviewShown: false
+        };
+    },
+    togglePreviewVisibility: function (newState) {
+        this.setState({
+            isPreviewShown: newState
+        });
+    },
+    changeCurrentItem: function (item) {
+        this.setState({
+            currentItem: item
+        });
+    },
+    addItem: function (item) {
+        this.setState({
+            allItems: this.state.allItems.concat([item])
+        });
     },
     render: function () {
         return React.createElement(
             'div',
-            { className: 'item-preview-screen fullscreen-popup' },
-            React.createElement(ItemEntity, this.props.item)
+            { id: 'home-page' },
+            React.createElement(ItemsList, { items: this.state.allItems, changeCurrentItem: this.changeCurrentItem, togglePreviewVisibility: this.togglePreviewVisibility }),
+            React.createElement(ItemPreviewContainer, { item: this.state.currentItem, togglePreviewVisibility: this.togglePreviewVisibility, isShown: this.state.isPreviewShown })
+        );
+    }
+});
+
+var Header = createReactClass({
+    displayName: 'Header',
+
+    render: function () {
+        return React.createElement(
+            'header',
+            { id: 'header-container' },
+            React.createElement(
+                'h1',
+                null,
+                'Welcome!'
+            )
         );
     }
 });
@@ -85,7 +180,12 @@ var ItemPreviewScreen = createReactClass({
  */
 var items = [{ id: 1, name: "Mobile Phone", description: "Brand new mobile phone", price: 750 }, { id: 2, name: "Jacket", description: "Brand new jacket", price: 75.50 }, { id: 3, name: "Car", description: "Brand new car", price: 7750 }, { id: 4, name: "Watch", description: "Brand new watch", price: 110.99 }];
 
-ReactDOM.render(React.createElement(ItemsList, { items: items }), document.getElementById('react-app'));
+ReactDOM.render(React.createElement(
+    'div',
+    { id: 'wrapper' },
+    React.createElement(Header, null),
+    React.createElement(HomePageView, null)
+), document.getElementById('react-app'));
 
 // var ContactItem = React.createClass({
 //     propTypes: {
--- scripts/app.jsx
+++ scripts/app.jsx
@@ -3,38 +3,48 @@ var STRINGS = {
     'currency': 'BGN'
 }
 
-var ItemEntity = createReactClass({
+var ItemListEntity = createReactClass({
     propTypes: {
         id: PropTypes.number.isRequired,
         name: PropTypes.string.isRequired,
         description: PropTypes.string,
-        price: PropTypes.number.isRequired
+        price: PropTypes.number.isRequired,
+        changeCurrentItem: PropTypes.func.isRequired,
+        togglePreviewVisibility: PropTypes.func.isRequired
+    },
+    changeItem: function () {
+        this.props.changeCurrentItem(this.props);
+        this.props.togglePreviewVisibility(true);
     },
     render: function () {
         return (
-            <li className='item'>
-                <h2 className='item-name'>{this.props.name}</h2>
+            <li className='item' onClick={this.changeItem} >
+                <h3 className='item-name'>{this.props.name}</h3>
                 <div className='item-description'>{this.props.description}</div>
                 <div className='item-price'>{this.props.price + ' ' + STRINGS['currency']}</div>
                 <a href={'products/' + this.props.id} className='item-link'>{STRINGS['view-more']}</a>
-            </li>
+            </li >
         )
     }
 });
 
 var ItemsList = createReactClass({
     propTypes: {
-        items: PropTypes.array.isRequired
+        items: PropTypes.array.isRequired,
+        changeCurrentItem: PropTypes.func.isRequired,
+        togglePreviewVisibility: PropTypes.func.isRequired,
     },
     render: function () {
-        var elementsToRender = this.props.items.map(function (item) {
+        var self = this;
+        var elementsToRender = self.props.items.map(function (item) {
             return (
-                <ItemEntity {...item} key={item.id} />
+                <ItemListEntity {...item} changeCurrentItem={self.props.changeCurrentItem} togglePreviewVisibility={self.props.togglePreviewVisibility} key={item.id} />
             );
         });
 
         return (
             <div className='items-list-container'>
+                <h2>Items List</h2>
                 <ul className='items-list'>
                     {elementsToRender}
                 </ul>
@@ -43,18 +53,77 @@ var ItemsList = createReactClass({
     }
 });
 
-var ItemPreviewScreen = createReactClass({
+var ItemPreviewContainer = createReactClass({
     propTypes: {
-        item: PropTypes.object.isRequired
+        item: PropTypes.object.isRequired,
+        isShown: PropTypes.bool.isRequired
+    },
+    getDefaultProps: function () {
+        return {
+            item: {},
+            isShown: false
+        }
+    },
+    render: function () {
+        if (this.props.isShown) {
+            return (
+                <div className='item-preview-screen fullscreen-popup'>
+                    <h2 className='item-name'>{this.props.item.name}</h2>
+                    <div className='item-description'>{this.props.item.description}</div>
+                    <div className='item-price'>{this.props.item.price + ' ' + STRINGS['currency']}</div>
+                    <a href={'products/' + this.props.item.id} className='item-link'>{STRINGS['view-more']}</a>
+                </div>
+            );
+        }
+        else {
+            return <div className='item-preview-screen fullscreen-popup' />;
+        }
+    }
+});
+
+var HomePageView = createReactClass({
+    getInitialState: function () {
+        return {
+            currentItem: {},
+            allItems: items,
+            isPreviewShown: false
+        }
+    },
+    togglePreviewVisibility: function (newState) {
+        this.setState({
+            isPreviewShown: newState
+        });
+    },
+    changeCurrentItem: function (item) {
+        this.setState({
+            currentItem: item
+        });
+    },
+    addItem: function (item) {
+        this.setState({
+            allItems: this.state.allItems.concat([item])
+        });
     },
     render: function () {
         return (
-            <div className='item-preview-screen fullscreen-popup'>
-                <ItemEntity {...this.props.item} />
+            <div id='home-page'>
+                <ItemsList {...{ items: this.state.allItems, changeCurrentItem: this.changeCurrentItem, togglePreviewVisibility: this.togglePreviewVisibility }} />
+                <ItemPreviewContainer item={this.state.currentItem} togglePreviewVisibility={this.togglePreviewVisibility} isShown={this.state.isPreviewShown} />
             </div>
-        );
+        )
     }
 });
+
+var Header = createReactClass({
+    render: function () {
+        return (
+            <header id="header-container">
+                <h1>Welcome!</h1>
+            </header>
+        )
+    }
+});
+
 
 /**
  * Data
@@ -67,7 +136,10 @@ var items = [
 ];
 
 ReactDOM.render(
-    <ItemsList {...{items: items}}/>,
+    <div id="wrapper">
+        <Header />
+        <HomePageView />
+    </div>,
     document.getElementById('react-app')
 );
 
